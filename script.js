9d62bbe33915d9f20abe05de2d944a4aa5a3f0df
Junaid Ul Haque sheikh <156045075+junaidsheikh0002@users.noreply.github.com>
2024-10-09 09:24:37 +0500

    https://github.com/junaidsheikh0002/Welcome-to-a-Jahangir-Khan-Baba-Chaat-House-Websites_0001/upload
    https://github.com/junaidsheikh0002/Welcome-to-a-Jahangir-Khan-Baba-Chaat-House-Websites_0001/upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Adding event listener to the button
+document.getElementById("viewWorkBtn").addEventListener("click", function() {
+    alert("Redirecting to the portfolio...");
+    // You can replace this with actual redirection logic
+    // window.location.href = '/portfolio';
+});
